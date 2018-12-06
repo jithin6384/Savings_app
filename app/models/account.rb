9793898288269751ea7866ef0ec5c5f870ad02ef
jithin <jeets2689@gmail.com>
2018-12-06 23:35:31 +0530

    s

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,7 @@
 class Account < ActiveRecord::Base
+  belongs_to :user
   validates :items_brought, presence: true
   validates :amount_spent, presence: true
-
+  validates :user_id, presence: true
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181205182312) do
+ActiveRecord::Schema.define(version: 20181206174835) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "items_brought"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20181205182312) do
     t.integer "total"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
